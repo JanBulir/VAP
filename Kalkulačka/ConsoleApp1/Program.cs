476f6a3f1a0082df5ded22722bf449ea79cbee88
JanBulir <147809266+JanBulir@users.noreply.github.com>
2024-03-22 11:12:44 +0100

    Add files via upload

---
+++ Kalkulačka/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿class ComplexNumber
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public ComplexNumber Add(ComplexNumber other)
+    {
+        return new ComplexNumber(Real + other.Real, Imaginary + other.Imaginary);
+    }
+
+    public ComplexNumber Subtract(ComplexNumber other)
+    {
+        return new ComplexNumber(Real - other.Real, Imaginary - other.Imaginary);
+    }
+}
